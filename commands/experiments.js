6d6787c4a6dc431ccd3760c408b79e4dedd5a691
Cytronicode <novodoodle@gmail.com>
2022-02-27 00:04:49 +0800

    Updated on the experiments command.

--- commands/experiments.js
+++ commands/experiments.js
@@ -1,10 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-});
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('experiments')
