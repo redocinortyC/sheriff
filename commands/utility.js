ef7376d54d2ab621ca4dcc5142527bd338668ab5
Peter (Cytronicode) <92656878+Cytronicode@users.noreply.github.com>
2022-03-03 13:22:42 +0000

    Seperated utility and fun commands.

--- commands/utility.js
+++ commands/utility.js
@@ -13,25 +13,7 @@ module.exports = {
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('ping')
-				.setDescription('Replies with \'pong\' and your current latency!'))
-
-		// Cryptocurrency info command
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('cryptocurrency')
-				.setDescription('Spew out some stats on cryptocurrencies.')
-				.addStringOption(option =>
-					option.setName('cryptocurrency')
-						.setDescription('The cryptocurrencies I currently support')
-						.setRequired(true)
-						// Some cryptocurrencies
-						.addChoice('Avalanche', 'avalanche-2')
-						.addChoice('Bitcoin', 'bitcoin')
-						.addChoice('Dogecoin', 'dogecoin')
-						.addChoice('Ethereum', 'ethereum')
-						.addChoice('Polkadot', 'polkadot')
-						.addChoice('Polygon', 'matic-network')
-						.addChoice('Solana', 'solana'))),
+				.setDescription('Replies with \'pong\' and your current latency!')),
 
 	async execute(interaction) {
 
@@ -39,29 +21,5 @@ module.exports = {
 			const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
 			interaction.editReply(`:ping_pong: Pong! Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
 		}
-
-		if (interaction.options.getSubcommand() === 'cryptocurrency') {
-			const id = interaction.options.getString('cryptocurrency');
-			
-			let response = null;
-	
-			new Promise(async (resolve, reject) => {
-			  try {
-			    response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`);
-			  } catch(ex) {
-			    response = null;
-			    // Error logging
-			    console.log(ex);
-			    reject(ex);
-			  }
-			  if (response) {
-			    // Upon success
-			    const json = response.data;
-					await interaction.reply("**Cryptocurrency data (" + id + ")**\n```json\n" + JSON.stringify(json, null, ' ') + "```\n*:lizard: Data provided by the CoinGecko data market APIs.*");
-			    resolve(json);
-			  }
-			});
-		}
-
 	},
 };
