8fa8d66cd89dcf9e4f8ff8276eac376c46034687
Peter (Cytronicode) <92656878+Cytronicode@users.noreply.github.com>
2022-03-07 22:26:40 +0000

    Added new experimental command, and another bot maintenance.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <div align="center">
   <br />
   <p>
-    <img src="/assets/sheriff.png" width="250" alt="Sheriff logo" />
+    <img src="assets/sheriff.png" width="250" alt="Sheriff logo" />
   </p>
   <br />
   <p>
@@ -13,13 +13,36 @@
 ## About
 
 Sheriff is a multipurpose Discord bot that utilizes [Node.js](https://nodejs.org) and the
-[Discord API](https://discord.com/developers/docs/intro). It is also the official Discord bot for the International Junior Honor Society (IJHS). It is owned by [Peter (also known as Cytronicode)](mailto:novodoodle@gmail.com), but it is maintained by the whole IJHS community.
+[Discord API](https://discord.com/developers/docs/intro). It is also the official Discord bot for the International Junior Honor Society (IJHS). It is owned by [Cytronicode](mailto:novodoodle@gmail.com), but it is maintained by the whole IJHS community.
 
 - Multipurpose
 - Community maintained
 - 100% slash command-based
 
 See [the features guide](https://github.com/cytronicode/sheriff/blob/main/features.md) if you'd like to get a glimpse into what Sheriff is capable of!
+
+## Features
+
+### Experimental commands
+- Bot reactions
+
+### Moderation commands
+- Kick
+- Ban and unban
+
+### Role commands
+- Add/give role
+
+### Utility commands
+- Ping
+
+### Fun commands
+- Cryptocurrency data
+
+### Info commands
+- About IJHS
+- About the SLC
+- About the committees
 
 ## Cloning the repository onto your local system
 
@@ -29,7 +52,7 @@ See [the features guide](https://github.com/cytronicode/sheriff/blob/main/featur
 $ git clone https://github.com/Cytronicode/sheriff.git
 ```
 
-Note that if you want to base your own bot off of this repository, you need to rename the `config-example.json` file to `config.json` and add the guild ID and client ID. Additionally, note that **the bot token is an environment variable.**
+Note that if you want to base your own bot off of this repository, you need to rename the `config-example.json` file to `config.json` and add the guild ID and client ID. Additionally, note that **the bot token is an environment variable**, so there is also a need for you to create a local `.env` file.
 
 ## Contributing
 
--- commands/experiments.js
+++ commands/experiments.js
@@ -1,5 +1,11 @@
 // Imports
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client, Intents, Permissions } = require('discord.js');
+
+// Discord client and its intents
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+});
 
 // Experimental commands module
 module.exports = {
@@ -11,13 +17,52 @@ module.exports = {
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('react')
-				.setDescription('Reacts to the message the bot sent.')),
+				.setDescription('Reacts to the message the bot sent.'))
+
+		// Set activity command
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('set')
+				.setDescription('Sets the bot\'s username and/or activity.')
+				.addStringOption(option => option.setName('username').setDescription('New username for the bot'))
+				.addStringOption(option =>
+					option.setName('activity-type')
+						.setDescription('Sets status to "Watching/Listening to/Competing in ..."')
+						.addChoice('Watching', 'WATCHING')
+						.addChoice('Listening to', 'LISTENING')
+						.addChoice('Competing in', 'COMPETING'))
+				.addStringOption(option => option.setName('activity').setDescription('New activity for the bot'))),
 
 	async execute(interaction) {
 
 		if (interaction.options.getSubcommand() === 'react') {
 			const message = await interaction.reply({ content: 'Experimenting with reactions...', fetchReply: true });
 			message.react('🚀');
+		}
+
+		/* 
+		NEW EXPERIMENTAL COMMAND: /EXPERIMENTS SET
+		STATUS: CURRENTLY IN DEVELOPMENT
+		Sets the bot's new username, activity, and activity type.
+		TODO: Test it out in Discord and debug if necessary.
+		*/
+
+		if (interaction.options.getSubcommand() === 'set') {
+			const username = interaction.options.getString('username');
+			const activityType = interaction.options.getString('activity-type');
+			const activity = interaction.options.getString('activity');
+			
+			if (username && interaction.member.permissions.has(Permissions.FLAGS.MANAGE_NICKNAMES)){
+				client.user.setUsername(username);
+			} else {
+				await interaction.reply({ content: ':space_invader: Oops! An error occured.', ephemeral: true });
+			}
+
+			if (activityType && activity && interaction.member.permissions.has(Permissions.FLAGS.MANAGE_NICKNAMES)){
+				client.user.setActivity(activity, { type: activityType });
+			} else {
+				await interaction.reply({ content: ':space_invader: Oops! An error occured.', ephemeral: true });
+			}
 		}
 
 	},
