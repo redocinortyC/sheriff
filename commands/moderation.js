f6e0410ddcdc796aabf865d29a9a3d22e4c31fb0
Mezo <dominik@koch-bautechnik.de>
2022-04-28 17:27:15 +0200

    Performance fix

--- commands/moderation.js
+++ commands/moderation.js
@@ -1,69 +1,100 @@
 // Imports
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Permissions } = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Permissions } = require("discord.js");
 
 // Moderation commands module
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('moderation')
-		.setDescription('The basic Discord moderator\'s kit.')
+  data: new SlashCommandBuilder()
+    .setName("moderation")
+    .setDescription("The basic Discord moderator's kit.")
 
-		// Kick command
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('kick')
-				.setDescription('Boot someone out of the server.')
-				.addUserOption(option => option.setName('user').setDescription('User to be kicked').setRequired(true)))
+    // Kick command
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("kick")
+        .setDescription("Boot someone out of the server.")
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("User to be kicked")
+            .setRequired(true)
+        )
+    )
 
-		// Ban command
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('ban')
-				.setDescription('Ban hammer activated and ready to bonk.')
-				.addUserOption(option => option.setName('user').setDescription('User to be banned').setRequired(true)))
+    // Ban command
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("ban")
+        .setDescription("Ban hammer activated and ready to bonk.")
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("User to be banned")
+            .setRequired(true)
+        )
+    )
 
-		// Unban command
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('unban')
-				.setDescription('You have been un-bonked by the hammer.')
-				.addUserOption(option => option.setName('id').setDescription('User ID to be unbanned').setRequired(true))),
+    // Unban command
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("unban")
+        .setDescription("You have been un-bonked by the hammer.")
+        .addUserOption((option) =>
+          option
+            .setName("id")
+            .setDescription("User ID to be unbanned")
+            .setRequired(true)
+        )
+    ),
 
-	async execute(interaction) {
+  async execute(interaction) {
+    if (interaction.options.getSubcommand() === "ban") {
+      const user = interaction.options.getUser("user");
 
-		if (interaction.options.getSubcommand() === 'ban') {
-			const user = interaction.options.getUser('user');
+      if (
+        user &&
+        interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)
+      ) {
+        interaction.guild.members.ban(user);
+        await interaction.reply(`:loudspeaker: ${user} has been banned.`);
+      } else {
+        await interaction.reply({
+          content: ":space_invader: Oops! An error occured.",
+          ephemeral: true,
+        });
+      }
+    }
 
-			if (user && interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
-				interaction.guild.members.ban(user);
-				await interaction.reply(`:loudspeaker: ${user} has been banned.`);
-			} else {
-				await interaction.reply({ content: ':space_invader: Oops! An error occured.', ephemeral: true });
-			}
-		}
+    if (interaction.options.getSubcommand() === "kick") {
+      const member = interaction.options.getMember("user");
 
-		if (interaction.options.getSubcommand() === 'kick') {
-			const member = interaction.options.getMember('user');
+      if (
+        member &&
+        interaction.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)
+      ) {
+        member.kick();
+        await interaction.reply(`:loudspeaker: ${member} has been kicked.`);
+      } else {
+        await interaction.reply({
+          content: ":space_invader: Oops! An error occured.",
+          ephemeral: true,
+        });
+      }
+    }
 
-			if (member && interaction.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) {
-				member.kick();
-				await interaction.reply(`:loudspeaker: ${member} has been kicked.`);
-			} else {
-				await interaction.reply({ content: ':space_invader: Oops! An error occured.', ephemeral: true });
-			}
-		}
+    if (interaction.options.getSubcommand() === "unban") {
+      const id = interaction.options.get("id")?.value;
+      const user = interaction.options.getUser("id");
 
-		if (interaction.options.getSubcommand() === 'unban') {
-			const id = interaction.options.get('id') ?.value;
-			const user = interaction.options.getUser('id');
-
-			if (interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
-				interaction.guild.members.unban(id);
-				await interaction.reply(`:loudspeaker: ${user} has been unbanned.`);
-			} else {
-				await interaction.reply({ content: ':space_invader: Oops! An error occured.', ephemeral: true });
-			}
-		}
-
-	},
+      if (interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+        interaction.guild.members.unban(id);
+        await interaction.reply(`:loudspeaker: ${user} has been unbanned.`);
+      } else {
+        await interaction.reply({
+          content: ":space_invader: Oops! An error occured.",
+          ephemeral: true,
+        });
+      }
+    }
+  },
 };
--- commands/role.js
+++ commands/role.js
@@ -1,33 +1,51 @@
 // Imports
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Permissions } = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Permissions } = require("discord.js");
 
 // Role commands module
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('role')
-		.setDescription('Everything about your roles in the server.')
+  data: new SlashCommandBuilder()
+    .setName("role")
+    .setDescription("Everything about your roles in the server.")
 
-		// Add role command
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('add')
-				.setDescription('Add another role to your growing profile.')
-				.addRoleOption(option => option.setName('role').setDescription('Role to be added').setRequired(true))
-				.addUserOption(option => option.setName('user').setDescription('User to be modified').setRequired(true))),
+    // Add role command
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("add")
+        .setDescription("Add another role to your growing profile.")
+        .addRoleOption((option) =>
+          option
+            .setName("role")
+            .setDescription("Role to be added")
+            .setRequired(true)
+        )
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("User to be modified")
+            .setRequired(true)
+        )
+    ),
 
-	async execute(interaction) {
+  async execute(interaction) {
+    if (interaction.options.getSubcommand() === "add") {
+      const role = interaction.options.getRole("role");
+      const user = interaction.options.getMember("user");
 
-		if (interaction.options.getSubcommand() === 'add') {
-			const role = interaction.options.getRole('role');
-			const user = interaction.options.getMember('user');
-
-			if (!user.roles.cache.has(role) && interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) {
-				user.roles.add(role);
-				await interaction.reply(`${user} has been crowned the ${role.name} role.`);
-			} else {
-				await interaction.reply({ content: ':space_invader: Oops! An error occured.', ephemeral: true });
-			}
-		}
-	},
+      if (
+        !user.roles.cache.has(role) &&
+        interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)
+      ) {
+        user.roles.add(role);
+        await interaction.reply(
+          `${user} has been crowned the ${role.name} role.`
+        );
+      } else {
+        await interaction.reply({
+          content: ":space_invader: Oops! An error occured.",
+          ephemeral: true,
+        });
+      }
+    }
+  },
 };
